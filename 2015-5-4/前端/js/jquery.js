58136d00fe50e81327cfcef263d255d054cc6fed
MarlonRay <slomo.zheng@hotmail.com>
2015-05-04 21:48:25 +0800

    5-4 前端

---
+++ 2015-5-4/前端/js/jquery.js
@@ -0,0 +1,30 @@
+$(document).ready(function($) {
+	$('.lanPos').css('display', 'none');
+	$(".left-side>ul>li").mouseover(function(event) {
+		 $(this).parents('ul').children('div.lanPos')
+			 .css('display', 'block');
+		$(this).children('ul.hide').stop().fadeIn();
+	});
+	$(".left-side>ul>li").mouseout(function(event) {
+		 $(this).parents('ul').children('.lanPos')
+		 	.css('display', 'none');
+		$(this).children('ul.hide').stop().fadeOut();
+	});
+	//$('.lanPos').css('top', $('.hover').offset().top - 123);
+	$('.left-side>ul>li').hover(function() {
+		$('.lanPos').css('top', $(this).offset().top - 123);
+	}, function() {
+		$('.lanPos').css('top', $('.hover').offset().top - 123);
+	})
+	// $('.left-side>ul>li').click(function() {
+	// 	for (var i = 0; i < $('.left-side>ul>li').size(); i++) {
+	// 		if (this == $('.left-side>ul>li').get(i)) {
+	// 			$('.left-side>ul>li')
+	// 				.eq(i).children('a').addClass('hover');
+	// 		} else {
+	// 			$('.left-side>ul>li')
+	// 				.eq(i).children('a').removeClass('hover');
+	// 		}
+	// 	}
+	// })
+});
